7e688f94a1166798fe9a7daea74c0819adf4e9db
ayushbhatt29 <abhatt.official@gmail.com>
2023-07-05 01:31:46 +0530

    fourth

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ayushbhatt29/puppy
 
 go 1.19
+
+require github.com/ayushbhatt29/dog v0.0.0-20230704194908-b99dbaee2d11
--- puppy.go
+++ puppy.go
@@ -1,9 +1,15 @@
 package puppy
 
+import "github.com/ayushbhatt29/dog"
+
 func Bark() string {
 	return "woof"
 }
 
 func Barks() string {
 	return "woof woof"
+}
+
+func BigBark() string {
+	return dog.WhenGrowUp(Bark())
 }
